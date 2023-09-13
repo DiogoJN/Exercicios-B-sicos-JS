78e94f0ed9d9e4373a2984ad2d5cf00803908eb0
Diogo Jesus <102834655+DiogoJN@users.noreply.github.com>
2023-09-13 15:15:31 -0300

    Add files via upload

---
+++ Desafio Intensivão JS/scripts/script1.js
@@ -0,0 +1,102 @@
+// Exercicio 1 Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// Resposta  
+//alert("Hello word") 
+ 
+
+
+// Exercicio 2 Crie um script que declare duas variáveis e exiba o resultado da soma entre elas
+
+// Resposta 
+// let num1 = 8, num2 = 9
+
+// let soma = num1 + num2
+
+// console.log(soma)
+
+
+
+// Exercicio 3 Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// Resposta 
+// let num = 'numero'
+
+// if (typeof(num) === 'number'){
+//   console.log("Essa variável é um número ")
+// } else {
+//   console.log("Essa variável não é um número ")
+// }
+
+
+
+// Exercicio 4 Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// Resposta
+// let nome = "Pedro"
+
+// if (typeof(nome) === 'string'){
+//   console.log("Essa variável é uma String ")
+// } else {
+//   console.log("Essa variável não é uma String ")
+// }
+
+
+
+// Exercicio 5 Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// Reposta
+// let verdadeiro = false
+
+// if (typeof(falso) === 'boolean'){
+//   console.log("Essa variável é booleana ")
+// } else {
+//   console.log("Essa variável não é boolena ")
+// }
+
+
+
+// Exercicio 6 Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// Resposta
+// let num1 = 15, num2 = 8
+
+// let sub = num1 - num2
+
+// console.log(sub)
+
+
+
+// Exercicio 7 Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// Resposta
+// let num1 = 15, num2 = 8
+
+// let mult = num1 * num2
+// console.log(mult)
+
+
+
+//Exercicio 8 Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// Resposta
+// let num1 = 15, num2 = 8
+
+// let div = num1 / num2
+
+// console.log(div)
+
+
+
+//Exercicio 9 e 10 
+// 9 - Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par"
+// 10 - Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+
+//Resposta
+// let num = 37
+
+// if(num%2 == 0) {
+//   console.log("Esse é um número par ")
+// } else {
+//   console.log("Esse é um número impar ")
+// }
